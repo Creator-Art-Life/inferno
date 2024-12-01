6351e70b6e7e1d38fe80fa46c0c30b9d543bb3c2
Creator-Art-Life <maximshool12@gmail.com>
2024-12-01 19:19:48 +0200

    first commit form new project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/global/navbar'
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center flex-col">
+      <Navbar />
+      ppp
+    </main>
+  )
+}
